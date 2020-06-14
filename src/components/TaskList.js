6a25ddcf604d556f16cdcd67df0d9d7b47b3112a
Chidi Orji <orjichidi95@gmail.com>
2020-06-14 22:46:29 +0100

    feat: build inbox screen

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,10 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import { Task } from "./Task";
+
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
 export const TaskList = (props) => {
   const { loading, tasks, onPinTask, onArchiveTask } = props;
@@ -65,3 +68,15 @@ TaskList.propTypes = {
 TaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(TaskList);
